af24334d107937bf896a9ffa2189b707d9ea04b4
Patrick Sodré <psodre@gmail.com>
2020-04-16 17:55:50 -0400

    Add run_exports to jemalloc
    Update license to BSD-2-Clause
    
    Use name jemalloc
    
    Update CI/CD backend to azure
    
    Add compiler toolchain
    
    Update build.sh to match master branch

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,10 +3,13 @@
 set -e
 set -x
 
-./autogen.sh
-./configure --prefix=$PREFIX
-# Skip doc generation as the required docbook XSLs are not yet packaged on conda-forge
-touch doc/jemalloc.html
-touch doc/jemalloc.3
+# Static TLS has caused users to experience some errors of the form
+# "libjemalloc.so.2: cannot allocate memory in static TLS block"
+#
+# We disable this feature until we better understand how to avoid loader errors
+# of this type
+./configure --prefix=$PREFIX \
+            --disable-tls \
+            --with-mangling=aligned_alloc:__aligned_alloc
 make -j${CPU_COUNT}
 make install
