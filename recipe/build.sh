8b16260f99f68efa42d135df0ab5502b587b0a56
Patrick Sodré <psodre@gmail.com>
2020-04-17 09:06:37 -0400

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,7 @@ set -x
 #
 # We disable this feature until we better understand how to avoid loader errors
 # of this type
+./autogen.sh
 ./configure --prefix=$PREFIX \
             --disable-tls \
             --with-mangling=aligned_alloc:__aligned_alloc
