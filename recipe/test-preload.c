a0f83cd9519848b84e6b4c8adf9d21c50ad45c7c
Matthias Diener <matthias.diener@gmail.com>
2023-02-19 20:29:32 -0600

    remove stats

--- recipe/test-preload.c
+++ recipe/test-preload.c
@@ -1,13 +1,9 @@
 #include <stdlib.h>
-#include <jemalloc/jemalloc.h>
 
 int main(void) {
 
   for (size_t i = 0; i < 1000; i++) {
     // Leak some memory
-    malloc(i * 100);
+    malloc(i * 1000);
   }
-
-  // Dump allocator statistics to stderr
-  malloc_stats_print(NULL, NULL, NULL);
 }
